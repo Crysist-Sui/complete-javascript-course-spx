124b7ed46e3d42f51bef82b78e64f4386d699b28
Suipengxiang <coolpengxiang@gmail.com>
2022-05-25 21:55:37 +0800

    P8 010 Values and Variables

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+console.log(40);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
