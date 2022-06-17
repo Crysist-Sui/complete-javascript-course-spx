d6ffcada0a2c98c16bf2af42728359b3ab6ac54a
Suipengxiang <coolpengxiang@gmail.com>
2022-06-17 21:34:41 +0800

    P10 013 let, const and var 已看完

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,22 +1,60 @@
-let js = "amazing";
-console.log(40);
+// let js = "amazing";
+// console.log(40);
+//
+// console.log("Jonas");
+// console.log(23);
+//
+// let firstName = "Jonas";
+// console.log(firstName);
+//
+// let jonas_matilda = "JM";
+// let $function = 27;
+//
+// let person = "jonas";
+// let PI = 3.1415;
+//
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+//
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+//
+// console.log(myFirstJob)
 
-console.log("Jonas");
-console.log(23);
+/*
+let javascriptIsFUn = true;
+console.log(javascriptIsFUn);
+// console.log(typeof true);
+console.log(typeof javascriptIsFUn);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
 
-let firstName = "Jonas";
-console.log(firstName);
+javascriptIsFUn = "YES!";
+console.log(typeof javascriptIsFUn);
 
-let jonas_matilda = "JM";
-let $function = 27;
+let year;
+console.log(year);
+console.log(typeof year);
 
-let person = "jonas";
-let PI = 3.1415;
+year = 1991;
+console.log(typeof year);
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+console.log(typeof null);
+*/
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+// let var const 区别
+// let 定义变量,const定义常量
+// var是定义变量的旧方法
+let age = 30;
+age = 31;
 
-console.log(myFirstJob)
+const birthYear = 1991;
+birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtman";
+console.log(lastName);
