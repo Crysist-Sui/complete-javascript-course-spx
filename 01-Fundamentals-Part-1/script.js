5946a269b26adbb73593a2a2af6b04881ef0cbfd
suipengxiang <coolpengxiang@gmail.com>
2022-08-20 16:05:37 +0800

    P19 022

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -84,10 +84,53 @@ const isFullAge = ageSarah >=18;
 console.log()
 */
 
-const firstName = 'Jonas';
-const job = 'teacher';
-const birthYear = 1991;
-const year = 2037;
+/*                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     */
 
-const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
-console.log(jonas);
+/*
+const age = 15;
+const isOldEnough = age >= 18;
+if (age >= 18) {
+    console.log("Sarah can start driving license🚗")
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+}
+*/
+
+
+/*
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+
+const inputYear = "1991";
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(String(1234))
+
+// type coercion
+console.log("I am " + String(23) + " years old");  // str(I am 23 years old)
+console.log("23" - "10" - 3);  // int(10)
+console.log("23" / "2");  // int(11.5)
+*/
+
+/*
+let n = '1' + 1 // "11"
+n = n - 1; // 10
+console.log(n);
+*/
+
+// 五种False类型的值：0, "", undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
